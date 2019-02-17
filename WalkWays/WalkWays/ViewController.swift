f6ba50c8891743d58bf2d588febd07a515ab2a99
Abhiram Santhosh <abhisanthosh@cmail.carleton.ca>
2019-02-17 15:04:35 -0500

    Added mapkit functionality

--- WalkWays/WalkWays/ViewController.swift
+++ WalkWays/WalkWays/ViewController.swift
@@ -7,14 +7,39 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    private let locationManager = CLLocationManager();
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        configureLocationServices();
     }
+    
+    
+
+    private func configureLocationServices(){
+        locationManager.delegate = self;
+        
+        if(CLLocationManager.authorizationStatus() == .notDetermined){
+            
+            locationManager.requestAlwaysAuthorization(); //allways using location - we can change this if we want
+        }
+    }
+}
 
 
+extension ViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        <#code#>
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        <#code#>
+    }
 }
 
